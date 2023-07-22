f77cc90a7ca3d8c7943676914073e338e5dfb56a
DigitalDeveloperAM <achrafmehenneche.am@gmail.com>
2023-07-22 15:43:37 +0200

    Completed assignemnt

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(Feet) {
   // Write your code here!
+
+  if (Feet <= 400) {
+    return "That will be twenty bucks.";
+  } else if (Feet >= 400 && Feet >= 2000) {
+    return "That will be twenty bucks.";
+  } else if (Feet >= 2000 && Feet <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return 'No can do.';
+  }
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(nyc) {
   // Write your code here!
+
+  return ncy === city ? "Ok, sounds good." : "No go.";
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip() {
   // Write your code here!
+
+  switch (generous) {
+    case "generous":
+      return "Thank you so much.";
+      break;
+    case "not as generous";
+      return "thank you.";
+      break;
+    default:
+      return "Bye";
+
+  }
 }
